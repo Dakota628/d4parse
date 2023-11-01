cefb25b4f368b987fc8841be9da28d3f9f28237d
Dakota <dakota@dasigns.net>
2023-10-31 21:30:00 -0400

    Add map to dpeloy script

--- cmd/deploy/deploy.go
+++ cmd/deploy/deploy.go
@@ -19,22 +19,33 @@ import (
 )
 
 const (
-	bucketName = "docs-diablo-parse"
-	accountId  = "4bc1086f05e53d1b4006b5edfb5b7732"
-
-	localBasePath = "docs"
-	workers       = 256
-	maxRetries    = 100
+	workers    = 256
+	maxRetries = 100
 )
 
-var (
-	awsBucketName = aws.String(bucketName)
-)
+var r2DeployConfigs = []R2DeployConfig{
+	{
+		AccountId:  "4bc1086f05e53d1b4006b5edfb5b7732",
+		BucketName: aws.String("docs-diablo-farm"),
+		LocalPath:  "docs",
+	},
+	{
+		AccountId:  "4bc1086f05e53d1b4006b5edfb5b7732",
+		BucketName: aws.String("map-diablo-farm"),
+		LocalPath:  filepath.Join("map", "dist"),
+	},
+}
 
-func getAwsConfig(accessKeyId string, secretAccessKey string) aws.Config {
+type R2DeployConfig struct {
+	AccountId  string
+	BucketName *string
+	LocalPath  string
+}
+
+func getAwsConfig(dc R2DeployConfig, accessKeyId string, secretAccessKey string) aws.Config {
 	r2Resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
 		return aws.Endpoint{
-			URL: fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountId),
+			URL: fmt.Sprintf("https://%s.r2.cloudflarestorage.com", dc.AccountId),
 		}, nil
 	})
 
@@ -54,20 +65,15 @@ func getAwsConfig(accessKeyId string, secretAccessKey string) aws.Config {
 	return cfg
 }
 
-func getFilesToUpload(skipSno bool) chan string {
+func getFilesToUpload(dc R2DeployConfig) chan string {
 	c := make(chan string, workers*10)
-	snoPrefix := filepath.Join("docs", "sno")
 
 	go func() {
 		err := godirwalk.Walk(
-			localBasePath,
+			dc.LocalPath,
 			&godirwalk.Options{
 				Callback: func(path string, de *godirwalk.Dirent) error {
 					if !de.IsRegular() {
-						return nil
-					}
-
-					if skipSno && filepath.HasPrefix(path, snoPrefix) {
 						return nil
 					}
 
@@ -88,8 +94,8 @@ func getFilesToUpload(skipSno bool) chan string {
 	return c
 }
 
-func getObjectKey(path string) string {
-	name, err := filepath.Rel(localBasePath, path)
+func getObjectKey(dc R2DeployConfig, path string) string {
+	name, err := filepath.Rel(dc.LocalPath, path)
 	if err != nil {
 		log.Fatalf("Failed to get object name for %q: %s", path, err)
 	}
@@ -108,12 +114,6 @@ func main() {
 		log.Fatal("Missing AWS_SECRET_ACCESS_KEY")
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	cfg := getAwsConfig(accessKeyId, secretAccessKey)
-	files := getFilesToUpload(false)
-
 	if err := mime.AddExtensionType(".bin", "application/octet-stream"); err != nil {
 		log.Fatalf("Failed to add .bin mime extension: %s", err)
 	}
@@ -121,54 +121,62 @@ func main() {
 		log.Fatalf("Failed to add .mpk mime extension: %s", err)
 	}
 
-	var count atomic.Uint64
-	wg := &sync.WaitGroup{}
-	for i := uint(0); i < workers; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			client := s3.NewFromConfig(cfg)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-			for {
-				path, ok := <-files
-				if !ok {
-					return
+	for _, dc := range r2DeployConfigs {
+		cfg := getAwsConfig(dc, accessKeyId, secretAccessKey)
+		files := getFilesToUpload(dc)
+
+		var count atomic.Uint64
+		wg := &sync.WaitGroup{}
+		for i := uint(0); i < workers; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				client := s3.NewFromConfig(cfg)
+
+				for {
+					path, ok := <-files
+					if !ok {
+						return
+					}
+
+					objectKey := getObjectKey(dc, path)
+					contentType := mime.TypeByExtension(filepath.Ext(path))
+
+					file, err := os.Open(path)
+					if err != nil {
+						log.Fatalf("Failed to open file %q: %s", path, err)
+					}
+
+					fileStat, err := file.Stat()
+					if err != nil {
+						log.Fatalf("Failed to stat file %q: %s", path, err)
+					}
+
+					params := &s3.PutObjectInput{
+						Bucket:        dc.BucketName,
+						Key:           aws.String(objectKey),
+						Body:          file,
+						ContentLength: fileStat.Size(),
+						ContentType:   aws.String(contentType),
+					}
+
+					for _, err = client.PutObject(ctx, params); err != nil; {
+						log.Printf("Retrying PutObject %q with new client due to error: %s", objectKey, err)
+						client = s3.NewFromConfig(cfg)
+						time.Sleep(1 * time.Second)
+					}
+
+					newCount := count.Add(1)
+					log.Printf("Uploaded %s->%s (#%d)", path, objectKey, newCount)
+
+					// TODO: track uploaded successfully files to allow continuing; don't re-upload same file
 				}
+			}()
+		}
 
-				objectKey := getObjectKey(path)
-				contentType := mime.TypeByExtension(filepath.Ext(path))
-
-				file, err := os.Open(path)
-				if err != nil {
-					log.Fatalf("Failed to open file %q: %s", path, err)
-				}
-
-				fileStat, err := file.Stat()
-				if err != nil {
-					log.Fatalf("Failed to stat file %q: %s", path, err)
-				}
-
-				params := &s3.PutObjectInput{
-					Bucket:        awsBucketName,
-					Key:           aws.String(objectKey),
-					Body:          file,
-					ContentLength: fileStat.Size(),
-					ContentType:   aws.String(contentType),
-				}
-
-				for _, err = client.PutObject(ctx, params); err != nil; {
-					log.Printf("Retrying PutObject %q with new client due to error: %s", objectKey, err)
-					client = s3.NewFromConfig(cfg)
-					time.Sleep(1 * time.Second)
-				}
-
-				newCount := count.Add(1)
-				log.Printf("Uploaded %s->%s (#%d)", path, objectKey, newCount)
-
-				// TODO: track uploaded successfully files to allow continuing
-			}
-		}()
+		wg.Wait()
 	}
-
-	wg.Wait()
 }
